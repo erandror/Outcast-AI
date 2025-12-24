015f21aa68b3fc6dd60546ac4dc15269cae97c2a
Eran Dror <eran.dror@gmail.com>
2025-12-24 16:45:53 -0500

    Add initial unit and UI tests for Outcast app
    - Created OutcastTests.swift for unit testing with a sample test case.
    - Added OutcastUITests.swift for UI testing, including setup, teardown, and example tests.
    - Introduced OutcastUITestsLaunchTests.swift to verify app launch and capture screenshots.

---
+++ OutcastTests/OutcastTests.swift
@@ -0,0 +1,16 @@
+//
+//  OutcastTests.swift
+//  OutcastTests
+//
+//  Created by Eran Dror on 12/24/25.
+//
+
+import Testing
+
+struct OutcastTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
